7e46a793731d4e2f4e84ca6489835b82e26e49c0
m05e5 <calebmoses1111@gmail.com>
2021-10-05 14:50:36 +0100

    add medical histories and invoices

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -10,6 +10,25 @@ CREATE TABLE patients(
 CREATE TABLE treatments(
  Id INT GENERATED ALWAYS AS IDENTITY,
  type Varchar(50),
- name Varchr(100),
+ name Varchar(100),
  PRIMARY KEY(Id)
 );
+
+CREATE TABLE medical_histories(
+  Id INT GENERATED ALWAYS AS IDENTITY,
+  admitted_at date,
+  status varchar(100),
+  patient_id int,
+  FOREIGN KEY(patient_id) REFERENCES patients(Id),
+  PRIMARY KEY(Id)
+);
+
+CREATE TABLE invoices (
+  Id INT GENERATED ALWAYS AS IDENTITY,
+  total_amount decimal,
+  generated_at date,
+  payed_at date,
+  medical_history_id INT,
+  FOREIGN KEY(medical_history_id) REFERENCES medical_histories(Id),
+  PRIMARY KEY(Id)
+)
